3b0730e7f2543d26e0fe156344b7bb9d5d3c183c
yuvachang <yuva.chang@gmail.com>
2019-10-17 20:26:42 -0400

    Updates

---
+++ precache-manifest.f9d74f44bd82489dc284b28c3e7e78e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8ccd99421762390a8050246260f8a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8544d6d8aad96a3657fa",
+    "url": "/static/js/2.a2e06159.chunk.js"
+  },
+  {
+    "revision": "4068241a0680cf20a177",
+    "url": "/static/js/main.5841c2b4.chunk.js"
+  },
+  {
+    "revision": "27734f08462e73f7a9f9",
+    "url": "/static/js/runtime-main.5c7d27b3.js"
+  }
+]);
